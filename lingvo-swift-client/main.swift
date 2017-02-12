6ce5618c258c0dfd513327b03970caeb46b107f1
daniel <dk@micromovie.com>
2017-02-12 12:36:08 +0100

    importing empty Xcode command line project

---
+++ lingvo-swift-client/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lingvo-swift-client
+//
+//  Created by Daniel Krüger on 12.02.17.
+//  Copyright © 2017 Lingvo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
